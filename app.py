dd805cb814b1402a54648625db53925410521af2
ubaidkhan08 <88443025+ubaidkhan08@users.noreply.github.com>
2022-06-13 05:36:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,8 +81,8 @@ def classify(Pc,SEX,AGE,SibSp,Parch,FARE,EMB):
     inputs = [[Pc,SEX,AGE,SibSp,Parch,FARE,EMB]]
 
     from joblib import dump, load
-    log_model = load('AdaBoost.joblib')
-    predictionn = log_model.predict(scaler.transform(inputs))
+    log_model = load('AdaBoost_unscaled.joblib')
+    predictionn = log_model.predict(inputs)
     
     if predictionn[0] == 1:
         return('YAY! YOU SURVIVED!')
